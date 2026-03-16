8cb1d5e91df68d0f785d162220a13b5a3d7045aa
rebecaaras <rebecaaraujo2013@gmail.com>
2026-03-16 17:10:36 -0300

    add Post class
    parse converts markdown file into a hash, then the Post obj, @body is in html

--- app.rb
+++ app.rb
@@ -3,24 +3,28 @@ require 'sinatra' # DSL (Domain Specific Language) for easily building rails web
 require 'erb' # templating language
 require 'kramdown'
 
-# class Post
-#   parsed_content = 'blablabla'
-# 
-#   # será que tem como eu parsear o html para um json e capturar o conteúdo de cada tag?
-#   @title
-#   @date
-#   @tags
-#   @slug
-#   @body
-# 
-#   def generate_slug;end 
-# 
-#   def self.all;end # itera sobre todos os arquivos em /posts
-# end
-# 
-# def load_posts
-#   Kramdown::Document.new(File::read("posts/example-post.md")).to_html
-# end
+class Post
+  attr_reader :title, :date, :body
+  
+  def initialize(post_hash)
+    @title = post_hash[:title]
+    @date = Date.parse(post_hash[:date])
+    @body = post_hash[:body]
+  end
+
+  def self.parse(file)
+    text = File.read(file).strip
+    meta, body = text.split('---')[1..2]
+
+    post_hash = {}
+    meta.lines.map(&:strip).reject(&:empty?).collect do |line|
+      k, v = line.split(":")
+      post_hash[k.strip.to_sym] = v.strip
+    end
+    post_hash[:body] = body.strip
+    new(post_hash)
+  end
+end
 
 get '/' do
   erb :index
